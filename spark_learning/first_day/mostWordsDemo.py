e0fd27d94575cd4be7e78f6272b7c10ddc76430c
wxmimperio <wxmimperio@outlook.com>
2016-02-22 17:31:28 +0800

    "add mostWordsDemo"

---
+++ spark_learning/first_day/mostWordsDemo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'wxmimperio'
+
+from pyspark import SparkContext,SparkConf
+from os import path
+import re
+
+conf = SparkConf().setAppName('mostWords').setMaster('local')
+sc = SparkContext(conf=conf)
+
+dirname = path.dirname(path.dirname(__file__))
+filename = 'MostWords.txt'
+#path = os.path.join(tempdir, "WordCount.txt")
+path = '/'.join([dirname,'doc',filename])
+
+textFile = sc.textFile(path)
+
+words_count =textFile.map(lambda line: len(re.split('\s.',line))).reduce(lambda a, b: a if(a > b) else b)
+
+#words_count = textFile.map(lambda line: len(line)).collect()
+
+print words_count
