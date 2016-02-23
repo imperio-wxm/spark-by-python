86e704de8984aa339ea8235d24c02ca14fe50d43
wxmimperio <wxmimperio@outlook.com>
2016-02-23 11:18:29 +0800

    "修改过滤正则表达式，过滤规则为空格和逗号分割"

--- spark_learning/first_day/mostWordsDemo.py
+++ spark_learning/first_day/mostWordsDemo.py
@@ -16,6 +16,8 @@ path = '/'.join([dirname,'doc',filename])
 
 textFile = sc.textFile(path)
 
+# 将每一行按分割长度进行map
+# reduce所有行里，len最长的map映射
 words_count =textFile.map(lambda line: len(re.split('(\\s*,\\s*|\\s+)',line))).reduce(lambda a, b: a if(a > b) else b)
 
 #words_count = textFile.map(lambda line: len(line)).collect()
