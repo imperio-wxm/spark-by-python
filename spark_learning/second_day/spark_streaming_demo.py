3d0178ec4a1b4474a1429ea83edacf06736b6aa4
wxmimperio <wxmimperio@outlook.com>
2016-03-03 09:33:15 +0800

    "add spark_streaming_demo.py"

---
+++ spark_learning/second_day/spark_streaming_demo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'wxmimperio'
+
+from pyspark import SparkContext,SparkConf
+from pyspark.streaming import StreamingContext
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("spark_streaming_demo").setMaster("local")
+    sc = SparkContext(conf=conf)
+    # 批次时间一秒
+    ssc = StreamingContext(sc, 3)
+
+    # 监听端口
+    lines = ssc.socketTextStream('localhost', 9999)
+    #lines = ssc.textFileStream('/home/jabo/software/streaming_test/')
+
+    # 每行按空格分割
+    wordCounts = lines.flatMap(lambda line: line.split(" "))\
+                  .map(lambda word: (word, 1))\
+                  .reduceByKey(lambda a, b: a+b)
+
+    wordCounts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
+
