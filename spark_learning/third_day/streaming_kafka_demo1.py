d0b78b9be834697950e54604cd61e4442c948f23
wxmimperio <wxmimperio@outlook.com>
2016-03-07 17:10:41 +0800

    "添加手动同步kafka offset"

--- spark_learning/third_day/streaming_kafka_demo1.py
+++ spark_learning/third_day/streaming_kafka_demo1.py
@@ -14,9 +14,10 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf)
     ssc = StreamingContext(sc, 5)
 
+    # topic、host:ip
     kvs = KafkaUtils.createDirectStream(ssc, ["realdata_receive"], {"metadata.broker.list": "192.168.108.222:9092"})
 
-    lines = kvs.map(lambda x: x[1])
+    lines = kvs.map(lambda x: (x, 1)).reduceByKey(lambda a, b: a+b)
     lines.pprint()
     # 每行按空格分割
     """
@@ -25,5 +26,20 @@ if __name__ == "__main__":
                   .reduceByKey(lambda a, b: a+b)
     wordCounts.pprint()
     """
+
+    # 手动同步kafka offset
+    offsetRanges = []
+
+    def storeOffsetRanges(rdd):
+         global offsetRanges
+         offsetRanges = rdd.offsetRanges()
+         return rdd
+
+    def printOffsetRanges(rdd):
+         for o in offsetRanges:
+             print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
+
+    kvs.transform(storeOffsetRanges).foreachRDD(printOffsetRanges)
+
     ssc.start()
     ssc.awaitTermination()
