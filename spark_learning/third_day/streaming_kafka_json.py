3a123da9efc0276d747f1af066da4093e80a47b5
wxmimperio <wxmimperio@outlook.com>
2016-03-08 15:26:52 +0800

    "toJson方法解析json"

--- spark_learning/third_day/streaming_kafka_json.py
+++ spark_learning/third_day/streaming_kafka_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 __author__ = 'wxmimperio'
 
 from pyspark import SparkContext, SparkConf
@@ -7,20 +7,31 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import json
 import sys
+from pyspark.sql import SQLContext
 
 if __name__ == "__main__":
     reload(sys)
-    sys.setdefaultencoding('utf-8')
+    sys.setdefaultencoding("utf-8")
+
     conf = SparkConf().setMaster("local[2]").setAppName("streaming_kafka_json")
     sc = SparkContext(conf=conf)
-    ssc = StreamingContext(sc, 3)
+    ssc = StreamingContext(sc, 5)
+
+    sqlC = SQLContext(sc)
 
     kvs = KafkaUtils.createDirectStream(ssc, ["realdata_receive"], {"metadata.broker.list": "192.168.108.222:9092"})
 
-    lines = kvs.map(lambda x: (x, 1)).reduceByKey(lambda a, b: a+b)
+
+    # 1. lines是一个元组，lines[1]取第二个元素
+    # 2. 默认为unicode编码，转换成utf-8
+    # 3. json.loads()转换成json
+    def toJson(lines):
+        line = json.loads(lines[1].encode("utf-8"))
+        print line["oid"]
+
+    lines = kvs.map(lambda x: toJson(x))
     lines.pprint()
 
-    # 手动同步kafka offset
     offsetRanges = []
 
     def storeOffsetRanges(rdd):
