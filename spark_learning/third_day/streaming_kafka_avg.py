b92921084cfad2c3cde90ae1861344dfc36f0f0b
wxmimperio <wxmimperio@outlook.com>
2016-03-09 09:52:54 +0800

    "封装各个方法"

--- spark_learning/third_day/streaming_kafka_avg.py
+++ spark_learning/third_day/streaming_kafka_avg.py
@@ -7,28 +7,38 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import sys
 
-if __name__ == "__main__":
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
-    conf = SparkConf().setAppName("streaming_kafka_avg").setMaster("local[2]")
-    sc = SparkContext(conf=conf)
-    ssc = StreamingContext(sc, 5)
 
-    kvs = KafkaUtils.createDirectStream(ssc, ["realdata_receive"], {"metadata.broker.list": "192.168.108.222:9092"})
-
-
+def ensureOffset(kvs):
     offsetRanges = []
 
     def storeOffsetRange(rdd):
         global offsetRanges
         offsetRanges = rdd.offsetRanges()
         return rdd
-
     def printOffsetRange(rdd):
         for o in offsetRanges:
             print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
 
     kvs.transform(storeOffsetRange).foreachRDD(printOffsetRange)
+
+def setDefaultEncoding():
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+
+def initSparkContext(appName, masterName, scTime):
+    conf = SparkConf().setAppName(appName).setMaster(masterName)
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, scTime)
+    return ssc
+
+if __name__ == "__main__":
+    setDefaultEncoding()
+    ssc = initSparkContext("streaming_kafka_avg", "local[2]", 5)
+
+    kvs = KafkaUtils.createDirectStream(ssc, ["realdata_receive"], {"metadata.broker.list": "192.168.108.222:9092"})
+    kvs.pprint()
+
+    ensureOffset(kvs=kvs)
 
     ssc.start()
     ssc.awaitTermination()
