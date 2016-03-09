7e6c1e07f5b99aa36b4dba427d4703d078dbab7a
wxmimperio <wxmimperio@outlook.com>
2016-03-09 17:33:35 +0800

    "not finish count"

--- spark_learning/third_day/streaming_kafka_avg.py
+++ spark_learning/third_day/streaming_kafka_avg.py
@@ -7,23 +7,31 @@ from pyspark.streaming.kafka import KafkaUtils
 from utils.default_utils import setDefaultEncoding, initSparkContext, ensureOffset
 import json
 
+count = 0
+
 def updateFun(newValues, runningCount):
     if runningCount is None:
         runningCount = 0
     return sum(newValues, runningCount)
 
-def avgFun(lines):
+def sumFun(lines):
+    global count
+    count += 1
+    print "count=========" + str(count)
+
     line = json.loads(lines[1].encode("UTF-8"))
     #print type(line["oid"].encode("utf-8"))
-    return [{"oid":line["oid"].encode("utf-8"),"value":line["value"]}]
-
+    # return {"oid":line["oid"].encode("utf-8"),"value":line["value"]}
+    return ("value", line["value"])
 
 if __name__ == "__main__":
     setDefaultEncoding()
     ssc = initSparkContext("streaming_kafka_avg", "local[2]", 5)
+    ssc.checkpoint("hdfs://localhost:9000/checkpiont/streaming_cp_log")
 
     kvs = KafkaUtils.createDirectStream(ssc, ["realdata_receive"], {"metadata.broker.list": "192.168.108.222:9092"})
-    kvs.map(lambda lines: avgFun(lines)).pprint()
+    kvs.map(lambda value: sumFun(value)).reduceByKey(lambda x,y: x+y).\
+        updateStateByKey(updateFun).pprint()
 
     ensureOffset(kvs=kvs)
 
