90812322dfd99b6d48e815194718d328e1f0704d
wxmimperio <wxmimperio@outlook.com>
2016-03-09 13:30:33 +0800

    "封装utils工具类"

---
+++ spark_learning/utils/default_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'wxmimperio'
+
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+import sys
+
+def ensureOffset(kvs):
+    offsetRanges = []
+
+    def storeOffsetRange(rdd):
+        global offsetRanges
+        offsetRanges = rdd.offsetRanges()
+        return rdd
+    def printOffsetRange(rdd):
+        for o in offsetRanges:
+            print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
+
+    kvs.transform(storeOffsetRange).foreachRDD(printOffsetRange)
+
+def setDefaultEncoding():
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+
+def initSparkContext(appName, masterName, scTime):
+    conf = SparkConf().setAppName(appName).setMaster(masterName)
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, scTime)
+    return ssc
