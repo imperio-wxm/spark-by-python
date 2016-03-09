7bb68ef9f17d2835cc72a1a974f58c18f9d8f1bf
wxmimperio <wxmimperio@outlook.com>
2016-03-09 13:31:22 +0800

    "update 13:31"

--- spark_learning/third_day/streaming_kafka_avg.py
+++ spark_learning/third_day/streaming_kafka_avg.py
@@ -2,34 +2,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wxmimperio'
 
-from pyspark import SparkContext, SparkConf
-from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-import sys
-
-
-def ensureOffset(kvs):
-    offsetRanges = []
-
-    def storeOffsetRange(rdd):
-        global offsetRanges
-        offsetRanges = rdd.offsetRanges()
-        return rdd
-    def printOffsetRange(rdd):
-        for o in offsetRanges:
-            print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
-
-    kvs.transform(storeOffsetRange).foreachRDD(printOffsetRange)
-
-def setDefaultEncoding():
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
-
-def initSparkContext(appName, masterName, scTime):
-    conf = SparkConf().setAppName(appName).setMaster(masterName)
-    sc = SparkContext(conf=conf)
-    ssc = StreamingContext(sc, scTime)
-    return ssc
+# utils.default_utils的import必须将utils文件夹复制到spark目录下的python文件夹内才能引用成功
+from utils.default_utils import setDefaultEncoding, initSparkContext, ensureOffset
 
 if __name__ == "__main__":
     setDefaultEncoding()
@@ -42,4 +17,5 @@ if __name__ == "__main__":
 
     ssc.start()
     ssc.awaitTermination()
+
 
