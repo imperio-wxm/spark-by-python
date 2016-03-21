be7d25c05dc8c2ed21b465055770cf0d801d5dce
wxmimperio <wxmimperio@outlook.com>
2016-03-21 13:49:42 +0800

    "finish streaming_kafka_deltaT.py 计算时间差"

--- spark_learning/third_day/streaming_kafka_deltaT.py
+++ spark_learning/third_day/streaming_kafka_deltaT.py
@@ -9,7 +9,60 @@ import json
 import datetime
 import time
 
-deltaT_dict = {}
+
+def toJson(rdd):
+    line = json.loads(rdd[1].encode("UTF-8"))
+    # print type(line["oid"].encode("utf-8"))
+
+    change_time = int(time.mktime(
+        time.strptime(
+            datetime.datetime.now().strftime("%Y-%m-%d") + " " + line["collect_time"].encode("utf-8"),
+            "%Y-%m-%d %H:%M:%S:%f"
+        )))
+    line["collect_time"] = change_time
+    return [line]
+
+
+# newValues新传进来的值
+# runningCount
+def updateFun(newValues, runningCount):
+    newValue = ""
+    oldValue = ""
+    print "**********************************"
+    if newValues:
+        newValues.sort(key=lambda k: (k.get("collect_time")), reverse=True)
+        # 对新进来的值进行过滤
+        newValue = newValues[0]
+        print newValue
+    print "=================================="
+    if runningCount is not None:
+        # 对旧值进行过滤
+        oldValue = runningCount[0]
+        print oldValue
+    print "++++++++++++++++++++++++++++++++++"
+    return (newValue, oldValue)
+
+
+def disconnection_patrol(lines):
+    if lines[0] and lines[1] != "":
+        print lines[0].get('collect_time')
+        print lines[1].get('collect_time')
+        if lines[0].get('collect_time') - lines[1].get('collect_time') > 5:
+            print "time out"
+    else:
+        print "time out"
+
+
+def foreachPartitionFun(rdd):
+    def partitionOfRecordsFun(rdd):
+        print "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        for item in itertools.chain(rdd):
+            print item[0], item[1]
+            print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            disconnection_patrol(item[1])
+
+    rdd.foreachPartition(partitionOfRecordsFun)
+
 
 if __name__ == "__main__":
     checkpoint_path = "hdfs://localhost:9000/checkpiont/streaming_cp_log"
@@ -20,7 +73,7 @@ if __name__ == "__main__":
     ssc = initStreamingContext("streaming_kafka_deltaT", "local[2]", 5)
     ssc.checkpoint(checkpoint_path)
 
-
+    """
     def deltaT(rdd):
         asset_attr_list = {}
         # line = rdd
@@ -41,79 +94,11 @@ if __name__ == "__main__":
             }
             asset_attr_list.update(change_dict)
         return line["oid"], asset_attr_list
-
-
-    def toJson(rdd):
-        line = json.loads(rdd[1].encode("UTF-8"))
-        # print type(line["oid"].encode("utf-8"))
-
-        chenge_time = int(time.mktime(
-            time.strptime(
-                datetime.datetime.now().strftime("%Y-%m-%d") + " " + line["collect_time"].encode("utf-8"),
-                "%Y-%m-%d %H:%M:%S:%f"
-            )))
-        line["collect_time"] = chenge_time
-        return [line]
-
-
-    # newValues新传进来的值
-    # runningCount
-    def updateFun(newValues, runningCount):
-        oldValues = ""
-        print "**********************************"
-
-        """
-        print newValues.sort(key=lambda k:
-            k[int(time.mktime(
-                time.strptime(
-                    datetime.datetime.now().strftime("%Y-%m-%d") + " " + newValues[0]["collect_time"].encode("utf-8"),
-                    "%Y-%m-%d %H:%M:%S:%f"
-                )))])
-        """
-        newValues.sort(key=lambda k: (k.get("collect_time")), reverse=True)
-        # 对新进来的值进行过滤
-        print newValues[0]
-        print "=================================="
-        if runningCount is not None:
-            # 对旧值进行过滤
-            oldValues = runningCount[0]
-            print runningCount[0]
-        print "++++++++++++++++++++++++++++++++++"
-
-        return (newValues[0], oldValues)
-
+    """
 
     kvs = KafkaUtils.createDirectStream(ssc, kafka_topic_list, broker_list_dit)
     events = kvs.flatMap(lambda lines: toJson(lines))
-    # deltaT = events.map(lambda x: (x["oid"], datetime.datetime.now().strftime("%Y-%m-%d") + " " + x["collect_time"].encode("utf-8")))
     deltaT = events.map(lambda x: (x["oid"], x))
-
-
-    # deltaT.pprint()
-    # deltaT.foreachRDD(foreachPartitionFun)
-
-    def foreachPartitionFun(rdd):
-        def partitionOfRecordsFun(rdd):
-            print "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-            global deltaT_dict
-            for item in itertools.chain(rdd):
-                print item[0], item[1]
-                """
-                change_dict = {
-                        item[0]:item[1]
-                }
-                if deltaT_dict.has_key(item[0]):
-                    print "you"
-                    deltaT_dict.update(change_dict)
-                else:
-                    print "mei"
-                    deltaT_dict[item[0]] = item[1]
-                    print deltaT_dict
-                """
-                # print deltaT_dict
-        rdd.foreachPartition(partitionOfRecordsFun)
-
-
     deltaT.updateStateByKey(updateFun).foreachRDD(foreachPartitionFun)
 
     ensureOffset(kvs=kvs)
