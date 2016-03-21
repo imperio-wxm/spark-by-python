dc1fd3caededf528d5e0d9f4d964fcec450cd6fa
wxmimperio <wxmimperio@outlook.com>
2016-03-21 15:30:33 +0800

    "finish streaming_kafka_deltaT.py 计算时间差"

--- spark_learning/third_day/streaming_kafka_deltaT.py
+++ spark_learning/third_day/streaming_kafka_deltaT.py
@@ -60,6 +60,28 @@ def foreachPartitionFun(rdd):
 
     rdd.foreachPartition(partitionOfRecordsFun)
 
+"""
+def deltaT(rdd):
+    asset_attr_list = {}
+    # line = rdd
+    line = json.loads(rdd[1].encode("UTF-8"))
+
+    if not asset_attr_list.has_key(line["oid"]):
+        asset_attr_list = line
+        # print asset_attr_list
+    else:
+        change_dict = {
+            "asset_id": line["asset_id"],
+            "attr_name": line["attr_name"],
+            "collect_time": line["collect_time"],
+            "attr_id": line["attr_id"],
+            "value": line["value"],
+            "warring_name": line["warring_name"],
+            "warring_state": line["warring_state"]
+        }
+        asset_attr_list.update(change_dict)
+    return line["oid"], asset_attr_list
+"""
 
 if __name__ == "__main__":
     checkpoint_path = "hdfs://localhost:9000/checkpiont/streaming_cp_log"
@@ -69,29 +91,6 @@ if __name__ == "__main__":
     setDefaultEncoding()
     ssc = initStreamingContext("streaming_kafka_deltaT", "local[2]", 5)
     ssc.checkpoint(checkpoint_path)
-
-    """
-    def deltaT(rdd):
-        asset_attr_list = {}
-        # line = rdd
-        line = json.loads(rdd[1].encode("UTF-8"))
-
-        if not asset_attr_list.has_key(line["oid"]):
-            asset_attr_list = line
-            # print asset_attr_list
-        else:
-            change_dict = {
-                "asset_id": line["asset_id"],
-                "attr_name": line["attr_name"],
-                "collect_time": line["collect_time"],
-                "attr_id": line["attr_id"],
-                "value": line["value"],
-                "warring_name": line["warring_name"],
-                "warring_state": line["warring_state"]
-            }
-            asset_attr_list.update(change_dict)
-        return line["oid"], asset_attr_list
-    """
 
     kvs = KafkaUtils.createDirectStream(ssc, kafka_topic_list, broker_list_dit)
     deltaT = kvs.flatMap(lambda lines: toJson(lines)).map(lambda x: (x["oid"], x)).\
