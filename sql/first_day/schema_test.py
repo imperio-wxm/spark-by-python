c0005a4620999a6e47b70c3a8b5088ae88e79b9a
wxmimperio <wxmimperio@outlook.com>
2016-04-11 15:53:02 +0800

    "update schema_test.py"

--- sql/first_day/schema_test.py
+++ sql/first_day/schema_test.py
@@ -19,17 +19,17 @@ if __name__ == "__main__":
     parts = lines.map(lambda lines: lines.split(","))
 
     # 隐射表间关系
-    book = parts.map(lambda book: Row(name=book[0], author=book[1], prize=float(book[2]), publish=book[3]))
+    book = parts.map(lambda book: Row(name=book[0], author=book[1], price=float(book[2]), publish=book[3]))
 
     # 转换成schema并注册
     schemaPeople = sqlContext.createDataFrame(book)
     schemaPeople.registerTempTable("book")
 
-    # 定义sqk语句
-    book = sqlContext.sql("SELECT * FROM book WHERE prize > 50.0 AND prize < 60")
+    # 定义sqk语句(查询prize在50、60之间的书)
+    book = sqlContext.sql("SELECT * FROM book WHERE price > 50.0 AND price < 60")
 
     # 查询结果进行隐射
-    bookMap = book.map(lambda books: (books.name, books.author, books.prize, books.publish))
+    bookMap = book.map(lambda books: (books.name, books.author, books.price, books.publish))
 
     for book in bookMap.collect():
-        print "|Name: " + book[0], "|Author: " + book[1], "|Prize: " + str(book[2]), "|Publish: " + book[3] + "|"
+        print "|Name: " + book[0], "|Author: " + book[1], "|Price: " + str(book[2]), "|Publish: " + book[3] + "|"
