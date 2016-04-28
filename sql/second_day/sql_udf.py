000ad8e5c657f3d7e2cf8c6337ba02caa27aacd2
wxmimperio <wxmimperio@outlook.com>
2016-04-28 14:18:35 +0800

    "update sql_udf.py"

--- sql/second_day/sql_udf.py
+++ sql/second_day/sql_udf.py
@@ -5,13 +5,14 @@ __author__ = 'wxmimperio'
 from pyspark import SparkContext, SparkConf
 from pyspark import SQLContext
 import os
+import json
 
 # 根据evaluation进行分类
 def name_place(name, place, price, evaluation):
     if evaluation <= 3:
-        return name + " general"
+        return name + "," + "general"
     elif evaluation > 3 and evaluation <=5:
-        return name + " good"
+        return name + "," + "good"
 
 
 if __name__ == "__main__":
@@ -28,10 +29,28 @@ if __name__ == "__main__":
     # UDF自定义函数
     sqlContext.registerFunction("name_place", name_place)
 
-    lengthRDD = sqlContext.sql("SELECT name_place(name, place, price,evaluation) AS name_place FROM json_book")
+    evalRDD = sqlContext.sql("SELECT name_place(name, place, price,evaluation) AS book_eval FROM json_book")
 
     #bookMap = lengthRDD.map(lambda books: (books.name, books.author, books.price, books.publish, books.place))
 
-    lengthRDD.show()
+    evalRDD.show()
+
+    # 查询结果进行隐射
+    bookMap = evalRDD.map(lambda books: (books.book_eval))
+
+    general_list = []
+    good_list = []
+
+    for book in bookMap.collect():
+        book = book.encode("utf-8").split(',')
+        print book[0], book[1]
+        if book[1] == "general":
+            general_list.append(book[0])
+        elif book[1] == "good":
+            good_list.append(book[0].decode("utf-8"))
+
+    for item in general_list:
+        print item
+    print good_list
 
     sc.stop()
