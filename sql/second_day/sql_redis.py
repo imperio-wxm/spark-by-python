ac73f2a393dda049f1c4eeecb8b6332da622b7e5
wxmimperio <wxmimperio@outlook.com>
2016-04-28 10:28:16 +0800

    "update sql_redis.py"

--- sql/second_day/sql_redis.py
+++ sql/second_day/sql_redis.py
@@ -4,66 +4,16 @@ __author__ = 'wxmimperio'
 
 from pyspark import SparkContext, SparkConf
 from pyspark import SQLContext
-import redis
 import time
 from datetime import datetime,timedelta
+from sql.utils.redis_conn import RedisCache
 import itertools
-
-
-class RedisDBConfig:
-    HOST = '192.168.108.222'
-    PORT = 6379
-    DBID = 0
-
-
-def operator_status(func):
-    def gen_status(*args, **kwargs):
-        error, result = None, None
-        try:
-            result = func(*args, **kwargs)
-        except Exception as e:
-            error = str(e)
-
-        return {'result': result, 'error': error}
-
-    return gen_status
-
-
-class RedisCache(object):
-    def __init__(self):
-        if not hasattr(RedisCache, 'pool'):
-            RedisCache.create_pool()
-        self._connection = redis.Redis(connection_pool=RedisCache.pool)
-
-    @staticmethod
-    def create_pool():
-        RedisCache.pool = redis.ConnectionPool(
-            host=RedisDBConfig.HOST,
-            port=RedisDBConfig.PORT,
-            db=RedisDBConfig.DBID)
-
-    @operator_status
-    def set_data(self, key, value):
-        return self._connection.set(key, value)
-
-    @operator_status
-    def zrange_by_score(self, key, start, end):
-        return self._connection.zrangebyscore(key, start, end)
-
-    @operator_status
-    def z_add(self, key, value, size):
-        """
-        value不能出现""，双引号
-        """
-        return self._connection.zadd(key, value, size)
-
 
 def print_fun(collect_asset):
     for item in collect_asset:
         pass
         #print "|Name: " + item[0], "|Value: " + str(item[1])
         #RedisCache().z_add("save_his", item[0].encode("utf-8"), item[1])
-
 
 if __name__ == "__main__":
     conf = SparkConf().setMaster("sql_redis").setMaster("local[2]")
@@ -73,7 +23,7 @@ if __name__ == "__main__":
     # 查询从当前时间开始前10s的数据
     now_datetime = datetime.now()
     end_time = int(time.mktime(now_datetime.timetuple()))
-    start_time = int(time.mktime((now_datetime - timedelta(minutes=250)).timetuple()))
+    start_time = int(time.mktime((now_datetime - timedelta(seconds=5)).timetuple()))
     print start_time
     print end_time
 
@@ -90,6 +40,7 @@ if __name__ == "__main__":
         print "|Time: " + row[0], "|Name: " + row[1], "|Oid: " + row[2], "|Value: " + str(row[3]) + "|"
 
         his.registerTempTable("his_data_zadd")
+        #sqlContext.cacheTable("his_data_zadd")
 
         #assets = sqlContext.sql("SELECT his.name, his.oid FROM his_data_zadd as his WHERE his.value > 200 AND his.oid < 3000000")
         #sql_str = "SELECT his.name, his.oid FROM (SELECT MAX(temp_t.value), temp_t.name, temp_t.oid FROM his_data_zadd AS temp_t) his"
@@ -102,6 +53,7 @@ if __name__ == "__main__":
         sql_str = 'SELECT * FROM his_data_zadd his_t ORDER BY his_t.value DESC LIMIT 1'
         #sql_str = 'SELECT * FROM (SELECT his_t.value,his_t.name FROM his_data_zadd his_t WHERE his_t.value > 100)'
         assets = sqlContext.sql(sql_str)
+        #assets.saveAsParquetFile("hdfs://localhost:9000/user/parquet_1")
 
         assets.show()
 
---
+++ sql/second_day/sql_udf.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'wxmimperio'
+
+from sql.utils.redis_conn import RedisCache
+
