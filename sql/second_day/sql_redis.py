f4d822e8955320053d5314094c03788dc544642f
wxmimperio <wxmimperio@outlook.com>
2016-04-28 08:59:37 +0800

    "update sql_redis.py"

--- sql/second_day/sql_redis.py
+++ sql/second_day/sql_redis.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     # 查询从当前时间开始前10s的数据
     now_datetime = datetime.now()
     end_time = int(time.mktime(now_datetime.timetuple()))
-    start_time = int(time.mktime((now_datetime - timedelta(seconds=10)).timetuple()))
+    start_time = int(time.mktime((now_datetime - timedelta(minutes=250)).timetuple()))
     print start_time
     print end_time
 
@@ -83,17 +83,24 @@ if __name__ == "__main__":
     if his_data['result']:
         hisRDD = sc.parallelize(his_data['result'])
         his = sqlContext.jsonRDD(hisRDD)
+        print his.count()
+
+
+        row = his.sort(his.value.desc()).first()
+        print "|Time: " + row[0], "|Name: " + row[1], "|Oid: " + row[2], "|Value: " + str(row[3]) + "|"
+
         his.registerTempTable("his_data_zadd")
 
         #assets = sqlContext.sql("SELECT his.name, his.oid FROM his_data_zadd as his WHERE his.value > 200 AND his.oid < 3000000")
         #sql_str = "SELECT his.name, his.oid FROM (SELECT MAX(temp_t.value), temp_t.name, temp_t.oid FROM his_data_zadd AS temp_t) his"
         #sql_str = "SELECT his.name, his.oid, his.value FROM his_data_zadd AS his ORDER BY his.value DESC LIMIT 10"
-        #sql_str = 'SELECT his.name, his.oid FROM his_data_zadd AS his WHERE his.value=(SELECT MAX(temp_t.value) FROM his_data_zadd AS temp_t)'
+        #sql_str = 'SELECT his.name, his.oid FROM his_data_zadd AS his WHERE EXISTS (SELECT MAX(temp_t.value) FROM his_data_zadd AS temp_t)'
         """
         Spark 1.5 does not support subquery.
         """
         #sql_str = 'SELECT MAX(his_t.value) as max_value FROM his_data_zadd AS his_t GROUP BY his_t.name,his_t.oid,his_t.collect_time,his_t.value'
         sql_str = 'SELECT * FROM his_data_zadd his_t ORDER BY his_t.value DESC LIMIT 1'
+        #sql_str = 'SELECT * FROM (SELECT his_t.value,his_t.name FROM his_data_zadd his_t WHERE his_t.value > 100)'
         assets = sqlContext.sql(sql_str)
 
         assets.show()
