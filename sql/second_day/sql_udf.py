d51bc0faefbc583d64b413ed7cd5e7a4ef6555a3
wxmimperio <wxmimperio@outlook.com>
2016-04-28 13:11:47 +0800

    "update sql_udf.py"

--- sql/second_day/sql_udf.py
+++ sql/second_day/sql_udf.py
@@ -2,5 +2,39 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wxmimperio'
 
-from sql.utils.redis_conn import RedisCache
+from pyspark import SparkContext, SparkConf
+from pyspark import SQLContext
+import os
+
+# 根据evaluation进行分类
+def name_place(name, place, price, evaluation):
+    if evaluation <= 3:
+        return name + " general"
+    elif evaluation > 3 and evaluation <=5:
+        return name + " good"
+
+
+if __name__ == "__main__":
+    conf = SparkConf().setMaster("local[2]").setAppName("sql_udf")
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+
+    json_path = os.path.abspath("../doc/book.json")
+
+    # json读取并隐射
+    json_df = sqlContext.read.json(json_path)
+    json_df.registerTempTable("json_book")
+
+    # UDF自定义函数
+    sqlContext.registerFunction("name_place", name_place)
+
+    lengthRDD = sqlContext.sql("SELECT name_place(name, place, price,evaluation) AS name_place FROM json_book")
+
+    #bookMap = lengthRDD.map(lambda books: (books.name, books.author, books.price, books.publish, books.place))
+
+    lengthRDD.show()
+
+    sc.stop()
+
+
 
