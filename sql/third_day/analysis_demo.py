36170d9f08fcf68080527b4604afe239dbbd4000
wxmimperio <wxmimperio@outlook.com>
2016-04-29 17:26:06 +0800

    "update analysis_demo.py"

--- sql/third_day/analysis_demo.py
+++ sql/third_day/analysis_demo.py
@@ -15,29 +15,13 @@ def analysis_email(email):
     return email.split("@")[1].split(".")[0]
 
 
-def analysis_username(username):
-    """
-    用户名长度计算
-    """
-    return len(username)
-
-
-def analysis_surname(realname):
-    """
-    用户名姓氏分割
-    """
-    return realname[0]
-
-
 if __name__ == "__main__":
-    conf = SparkConf().setAppName("analysis_demo")
+    conf = SparkConf().setAppName("analysis_demo").setMaster("local[2]")
     sc = SparkContext(conf=conf)
     sqlContext = SQLContext(sc)
 
     # UDF自定义函数注册
     sqlContext.registerFunction("analysis_email", analysis_email)
-    sqlContext.registerFunction("analysis_username", analysis_username)
-    sqlContext.registerFunction("analysis_surname", analysis_surname)
 
     file_path = os.path.abspath("../doc/analysis.txt")
     lines = sc.textFile(file_path)
@@ -54,7 +38,6 @@ if __name__ == "__main__":
     """
     :邮箱分析与统计
     """
-    """
     email_str = "SELECT analysis_email(email) AS email FROM information"
     emailSQL = sqlContext.sql(email_str)
     # 求总数
@@ -63,27 +46,25 @@ if __name__ == "__main__":
     emailCollect = emailSQL.groupBy("email").count().collect()
     # email分析结果
     result_email(emailCollect, count)
-    """
-
 
     """
     :用户名与姓名分析与统计
     """
     """
     # 用户名长度统计
-    username_len_str = "SELECT analysis_username(username) AS username_len FROM information"
+    username_len_str = "SELECT LENGTH(username) AS username_len FROM information"
     usernameSQL = sqlContext.sql(username_len_str)
     usernameLenCollect = usernameSQL.groupBy("username_len").count().collect()
     result_username_len(usernameLenCollect, count)
 
     # 姓氏统计
-    surname_sql = "SELECT analysis_surname(realname) AS surname FROM information"
+    surname_sql = "SELECT SUBSTRING(realname,0,1) AS surname FROM information"
     surnameSQL = sqlContext.sql(surname_sql)
     surnameCollect = sorted(surnameSQL.groupBy("surname").count().collect(), key=(lambda x: x[1]), reverse=True)
     result_surname(surnameCollect, count)
 
     # 姓名长度统计
-    realname_sql = "SELECT analysis_username(realname) AS realname_len FROM information"
+    realname_sql = "SELECT LENGTH(realname) AS realname_len FROM information"
     realnameSQL = sqlContext.sql(realname_sql)
     realnameLenCollect = realnameSQL.groupBy("realname_len").count().collect()
     result_realname(realnameLenCollect, count)
@@ -92,13 +73,23 @@ if __name__ == "__main__":
     """
     :身份证分析与统计
     """
-    # 用户名长度统计
-    idcard_len_str = "SELECT analysis_username(idcard) AS idcard_len FROM information"
-    idcardSQL = sqlContext.sql(idcard_len_str)
-    temp_count = idcardSQL.groupBy("idcard_len").count()
+    """
+    # 身份证长度统计
+    idcard_len_str = "SELECT LENGTH(idcard) AS idcard_len FROM information"
+    idcardLenSQL = sqlContext.sql(idcard_len_str)
+    temp_count = idcardLenSQL.groupBy("idcard_len").count()
     idcardLenCollect = temp_count.filter(temp_count["idcard_len"] == '18').collect()
     print idcardLenCollect
     #result_username_len(idcardLenCollect, count)
+    """
+
+    idcard_str = "SELECT SUBSTRING(idcard,0,2) AS province_code,SUBSTRING(idcard,7,8) AS birthday, " \
+                 "SUBSTRING(idcard,7,4) AS birth_year,SUBSTRING(idcard,11,2) AS birth_month," \
+                 "SUBSTRING(idcard,13,2) AS birth_day,SUBSTRING(idcard,17,1) AS gender " \
+                 "FROM information WHERE LENGTH(idcard)='18'"
+    idcardSQL = sqlContext.sql(idcard_str)
+    provinceCollect = idcardSQL.groupBy("province_code").count().collect()
+    result_privince(provinceCollect, count)
 
     schemaInfo.show()
 
